04ea101a698c6a76d4363e194d0a3985fb1f34e7
Joe Drago <joedrago@gmail.com>
2025-03-23 11:34:21 -0700

    document convenience shell script

--- wabits_ffmpeg/main.c
+++ wabits_ffmpeg/main.c
@@ -16,6 +16,23 @@
 //
 // If anything I'm borrowing BSD code from myself, heh.
 
+// #!/bin/bash
+//
+// PIPELINE="-y -f avfoundation -i $1 -vf crop=60:60:2209:9"
+//
+// if [ -z "$1" ]; then
+//     ffmpeg -f avfoundation -list_devices true -i ""
+//     exit
+// fi
+//
+// if [ ! -z "$2" ]; then
+//     echo "Creating test capture PNG: $2"
+//     ffmpeg ${PIPELINE} -vframes 1 "$2"
+//     exit
+// fi
+//
+// ffmpeg ${PIPELINE} -f yuv4mpegpipe -r 5 - | /Users/joe/work/wabits/build/wabits
+
 typedef int wabitsBool;
 #define WABITS_TRUE 1
 #define WABITS_FALSE 0
